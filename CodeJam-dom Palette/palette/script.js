a762d8c0a57474eced627b9cb145744b78e2b598
Sergey Xursevich <hursik94@gmail.com>
2019-04-26 23:42:55 +0300

    add: start dev drag-on-drop

--- CodeJam-dom Palette/palette/script.js
+++ CodeJam-dom Palette/palette/script.js
@@ -4,12 +4,13 @@ window.onload = function () {
     let picker = document.querySelector(".pallete__colors");
     let key = null;
     let colorCurrent = getComputedStyle(document.querySelector('.tools__list__color-1')).backgroundColor;
+    let e = null;
 
     canvas.addEventListener('click', (event) => {
         let target = event.target;
 
-        while(target != canvas) {
-            if(target.tagName == "DIV") {
+        while (target != canvas) {
+            if (target.tagName == "DIV") {
                 switch (key) {
                     case 'transform':
                         transformElement(target);
@@ -20,6 +21,9 @@ window.onload = function () {
                     case 'bucket':
                         paintShape(target);
                         break;
+                    case 'move':
+                        moveShape(target);
+                        break;
                 }
             }
             target = target.parentNode;
@@ -29,20 +33,14 @@ window.onload = function () {
     picker.addEventListener('click', (event) => {
         let target = event.target;
 
-        while(target != picker) {
-            if(target.tagName == "DIV") {
+        while (target != picker) {
+            if (target.tagName == "DIV") {
                 changeColor(target);
                 return;
             }
             target = target.parentNode;
         }
     });
-
-
-
-    function transformElement(node) {
-      alert("Click");
-    }
 
     function Tools(elem) {
         this.bucket = function () {
@@ -56,7 +54,10 @@ window.onload = function () {
             picker.style.display = "block";
         };
         this.move = function () {
-            alert('move');
+            document.body.style.cssText = 'cursor: move';
+            key = 'move';
+            picker.style.display = "none";
+            e = elem;
         };
         this.transform = function () {
             document.body.style.cssText = 'cursor: pointer';
@@ -80,8 +81,8 @@ window.onload = function () {
 
     function transformElement(node) {
         console.log(node.style.borderRadius);
-        
-        if(!node.style.borderRadius || node.style.borderRadius == "0%") {
+
+        if (!node.style.borderRadius || node.style.borderRadius == "0%") {
             node.style.borderRadius = "50%";
         } else {
             node.style.borderRadius = "0%";
@@ -90,10 +91,10 @@ window.onload = function () {
 
     function changeColor(node) {
         const tmp = colorCurrent;
-        if(colorCurrent === getComputedStyle(node).backgroundColor) return;
-        
+        if (colorCurrent === getComputedStyle(node).backgroundColor) return;
+
         console.log(node.firstElementChild);
-        
+
 
         document.querySelector('.tools__list__color-1').style.backgroundColor = getComputedStyle(node).backgroundColor;
         colorCurrent = getComputedStyle(document.querySelector('.tools__list__color-1')).backgroundColor;
@@ -102,5 +103,31 @@ window.onload = function () {
 
     function paintShape(node) {
         node.style.backgroundColor = colorCurrent;
+    }
+
+    function moveShape(node) {
+        node.className = "item-move";
+        moveAt(event);
+        node.style.zIndex = 1000;
+
+        function moveAt(e) {
+            node.style.left = e.pageX - node.offsetWidth / 2 + 'px';
+            node.style.top = e.pageY - node.offsetHeight / 2 + 'px';
+        }
+
+
+        canvas.addEventListener('mousemove', moveAt(e));
+        canvas.addEventListener('mouseup', () => {
+            canvas.removeEventListener('mousemove', moveAt(e));
+        });
+
+        // canvas.onmousemove = function (e) {
+        //     moveAt(e);
+        // }
+
+        // canvas.onmouseup = function () {
+        //     canvas.onmousemove = null;
+        //     node.onmouseup = null;
+        // }
     }
 };
